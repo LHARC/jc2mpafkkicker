b267c9a77566b5cb383bddce0668166f41e60eb4
Stoppered <lucas_jumpfull@hotmail.com>
2013-12-23 01:04:15 -0200

    Update afk.lua

--- afk/server/afk.lua
+++ afk/server/afk.lua
@@ -1,64 +1,82 @@
--- * Anti AFK 0.1 * --
+-- * Anti AFK 0.2 * --
 -- * Written by Lucas H. (Stoppered) Special thanks to jmaz * --
 -- * Contact-me at webmaster@whispers.com.br --*/
 -- * http://www.cshpforum.com * --
 
 
 -- Conigure here
+ 
+local players = {}
+local oldtime = os.time()
 
-MaxTime = 15 -- Max time without move to get afk and be kicked in seconds (300 Seconds = 5 Minutes)
+-- Configure Here
+ 
+MaxTime = 300 -- Put the seconds to player get alert and be kicked from the server
+
+-- End the configuration
 
 
--- End the configure 
-
-players = {}
-oldtime = os.time()
 ApproachingKick =  math.floor(80/100*MaxTime)
-RemainderTime = math.floor(20/100*MaxTime)
-
-message = (" was kicked for inactivity [AFK]")
-messagemove = ("[SERVER] Move or will be kicked for inactivity in ")
-
-
-
-
-mainLoop = function()
-
-	if os.time() - oldtime >= 1 then 
-		for player in Server:GetPlayers() do
-			if players[player:GetId()] == nil then
-				players[player:GetId()] = { pos = player:GetPosition(), idletime = 0 }
-			else
-				if (players[player:GetId()].pos - player:GetPosition()):Length() < 0.1 then
-					players[player:GetId()].idletime = players[player:GetId()].idletime + 1
-				else
-					players[player:GetId()].idletime = 0
-				end
-
-				players[player:GetId()].pos = player:GetPosition()
-			end
-		end
-
-		for k,v in pairs(players) do
-			for player in Server:GetPlayers() do
-		CountDown = MaxTime - v.idletime
-		
-		 if v.idletime >= ApproachingKick then
-		 Chat:Send(player, messagemove ..CountDown.. " seconds", Color(255, 0, 4))
-		 end 
-		 
-			if v.idletime >= MaxTime then
-		    Chat:Broadcast("[Server] " .. player:GetName() .. message, Color(255, 0, 4))
-			print(player:GetName() .. message)
-			player:Kick(message) 
-			players[player:GetId()].idletime = 0
-
-			end
-		end
-	end
-	end
-
-	oldtime = os.time()
-
+message = (" was kicked for inactivity [AFK]") -- Console message and server Message
+messagemove = ("[SERVER] Move or will be kicked for inactivity in ") -- Message sended to a player about [AFK]
+ 
+local mainLoop = function()
+ 
+        if os.time() - oldtime >= 1 then
+ 
+                for k,v in pairs(players) do
+                        local plr = Player.GetById(k)
+ 
+                        if plr ~= nil then
+                                --print(Player.GetById(k):GetName()..": "..v.idletime) -- For see in realtime counting player position AFK on console
+                        end
+                end
+ 
+                for player in Server:GetPlayers() do
+                        if players[player:GetId()] ~= nil then
+                                if (players[player:GetId()].pos - player:GetPosition()):Length() < 0.1 then
+                                        players[player:GetId()].idletime = players[player:GetId()].idletime + 1
+                                else
+                                        players[player:GetId()].idletime = 0
+                                end
+ 
+                                players[player:GetId()].pos = player:GetPosition()
+                        end
+                end
+ 
+                for k,v in pairs(players) do
+                        local plr = Player.GetById(k)
+ 
+                        if plr ~= nil then
+                                CountDown = MaxTime - v.idletime
+ 
+                                if v.idletime >= ApproachingKick then
+                                        Chat:Send(Player.GetById(k), messagemove ..CountDown.. " seconds", Color(255, 0, 4))
+                                end
+ 
+                                if CountDown <= 0 then
+								        Chat:Broadcast("[SERVER] " ..Player.GetById(k):GetName().. message, Color(255, 0, 4))
+										print(Player.GetById(k):GetName().. " Player was kicked [afk]")
+                                       Player.GetById(k):Kick(message)
+                                end
+                        end
+                end
+ 
+ 
+        end
+ 
+ 
+        oldtime = os.time()
+ 
 end
+ 
+function plrquit(args)
+        table.remove(players, args.player:GetId())
+end
+function plrjoin(args)
+        players[args.player:GetId()] = { pos = args.player:GetPosition(), idletime = 0 }
+end
+ 
 Events:Subscribe("PostTick", mainLoop)
+Events:Subscribe("PlayerQuit", plrquit)
+Events:Subscribe("PlayerJoin", plrjoin)
